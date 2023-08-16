18a809d8c75ab4b9f2310b9fb3d2b6c87053795f
anas.falau <anas.falau@gmail.com>
2023-08-16 05:21:27 +0000

    debug

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,8 +4,8 @@
  */
 
 /**
- * main - function that creates an array of chars, 
- * 	  and initializes it with a specific char.
+ * create_array - function that creates an array of chars,
+ *		and initializes it with a specific char.
  * Description: A program that creates an array followed by a new line.
  * @size: arg
  * @c: arg
@@ -19,17 +19,17 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 	char *c_add;
+
 	c_add = malloc(4 * size);
 
 	if (size == 0)
-		return NULL;
-	
-	while(i < size)
+		return (NULL);
+
+	while (i < size)
 	{
 		c_add[i] = c;
 		i++;
 	}
-	
-	return c_add;
+	return (c_add);
 
 }
